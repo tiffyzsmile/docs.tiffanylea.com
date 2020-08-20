a1d41f4a669ebdb7338764181df0fe889f1dd4d4
tiffany may <tiffyzsmile@gmail.com>
2020-08-20 14:59:09 -0700

    prettier format updates

--- doczrc.js
+++ doczrc.js
@@ -1,10 +1,16 @@
 // doczrc.js
 export default {
-    dest: 'dist',
-    title: 'Tiffany Lea KB',
-    ignore: ['README.md', 'changelog.md', 'code_of_conduct.md', 'contributing.md', 'license.md'],
-    menu: [
-        'Overview',
-//        { name: 'Overview1', menu: ['Todos'] }
-    ]
-}
+  dest: "dist",
+  title: "Tiffany Lea KB",
+  ignore: [
+    "README.md",
+    "changelog.md",
+    "code_of_conduct.md",
+    "contributing.md",
+    "license.md",
+  ],
+  menu: [
+    "Overview",
+    //        { name: 'Overview1', menu: ['Todos'] }
+  ],
+};
