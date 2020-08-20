51555b280e3bf63f9d7735ed94aad3a96631606d
tiffany may <tiffyzsmile@gmail.com>
2020-08-20 09:11:06 -0700

    updated build folder to 'dist' and added to gitignore

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,6 @@
 // doczrc.js
 export default {
+    dest: 'dist',
     title: 'Tiffany Lea KB',
     ignore: ['README.md', 'changelog.md', 'code_of_conduct.md', 'contributing.md', 'license.md'],
     menu: [
