879ae2635ffb51e883f85ef96877c167eee0c3ec
tiffany may <tiffyzsmile@gmail.com>
2020-08-20 17:04:07 -0700

    updated ExternalLink to avoid having extra P tag output

--- src/components/ExternalLink.jsx
+++ src/components/ExternalLink.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { MdOpenInNew } from "react-icons/md";
 
-const ExternalLink = ({ children, ...props }) => (
-  <a target="_blank" rel="noopener noreferrer" {...props}>
-    {children} <MdOpenInNew size="0.8em" />
-  </a>
-);
+const ExternalLink = ({ href, text }) => {
+  return (
+    <a target="_blank" rel="noopener noreferrer" href={href}>
+      {text} <MdOpenInNew size="0.8em" />
+    </a>
+  );
+};
 
 export default ExternalLink;
