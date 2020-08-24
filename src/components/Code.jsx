224f444d4efc71ba3aef274c18d1ef3c85e04672
tiffany may <tiffyzsmile@gmail.com>
2020-08-24 13:24:19 -0700

    added margin to code element

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const styles = {
   padding: "16px 16px 0 16px",
+  margin: "32px 0",
   backgroundColor: "#f6f8fa",
   overflow: "hidden", // to respect child element margin
 };
